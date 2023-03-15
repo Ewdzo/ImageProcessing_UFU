54e4460ad3f4fc423be7cdfc6fdf741af9cb0688
Enzo Weder <enzowederma@gmail.com>
2023-03-15 19:16:38 -0300

    🎉 init: first commit

---
+++ A1/A1_EnzoWeder_Amostragem.m
@@ -0,0 +1,24 @@
+ogImg = imread('relogio.tif');
+
+targetDpi = 300;
+dpiRatio = round(1250 / targetDpi);
+
+
+newImgH = round(size(ogImg,1) / dpiRatio);
+newImgW = round(size(ogImg,2) / dpiRatio);
+newImg = zeros(newImgH, newImgW);
+
+k = 1;
+l = 1;
+
+for i=1:dpiRatio:size(ogImg,1)
+  l=1;
+    for j=1:dpiRatio:size(ogImg,2)
+    newImg(k,l) = ogImg(i,j);
+    l++;
+  endfor
+    k++;
+endfor
+
+subplot(1,2,1); imshow(ogImg); title('Original Picture');
+subplot(1,2,2); imshow(newImg); title('Resized Picture');
