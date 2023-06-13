cc3e110e509acccd6e5df3d5847d1f48ce979b26
Enzo Weder <enzowederma@gmail.com>
2023-06-12 23:37:47 -0300

    🐞 fix: restore blueValue and greenValue

--- TRAB_FINAL/TRAB_FINAL.m
+++ TRAB_FINAL/TRAB_FINAL.m
@@ -2,7 +2,6 @@ pkg load image
 
 img = imread("leaf.jpg");
 img = imresize(img, 0.03);
-
 img = padarray(img, [4,4]);
 
 newImg = zeros(size(img));
@@ -10,9 +9,13 @@ skeletonImg = zeros(size(newImg,1), size(newImg,2));
 
 for j=5:(size(img,1) - 4)
   for k=5:(size(img,2) - 4)
-      if(blueValue < (0.8 * greenValue))
-        newImg(j,k,:) = img(j,k,:);
-      endif
+    
+    greenValue = img(j,k,2);
+    blueValue = img(j,k,3);
+
+    if(blueValue < (0.8 * greenValue))
+      newImg(j,k,:) = img(j,k,:);
+    endif
   endfor
 endfor
 
@@ -22,10 +25,6 @@ newImg = zeros(size(img));
 for j=5:(size(img,1) - 4)
   for k=5:(size(img,2) - 4)
       smoothValue = smoothness(img, j, k);
-
-      redValue = img(j,k,1);
-      greenValue = img(j,k,2);
-      blueValue = img(j,k,3);
 
       if(smoothValue < 12)
         newImg(j,k,:) = img(j,k,:);
